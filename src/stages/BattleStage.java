c552a87f3ff2ed5563ae7291d876b4aacf496f1d
leejunho0622 <lee49409780@gmail.com>
2024-11-18 16:55:59 +0900

    Add setMonster(), printBattleMain()

--- src/stages/BattleStage.java
+++ src/stages/BattleStage.java
@@ -1,5 +1,49 @@
 package stages;
 
-public class BattleStage extends Stage{
+import java.io.IOException;
+import java.util.ArrayList;
 
+import interfaces.IOManager;
+import interfaces.MonsterManager;
+import textrpg.TextRPG;
+import units.Monster;
+
+public class BattleStage extends Stage implements MonsterManager{
+	private ArrayList<Monster> monsterList = new ArrayList<Monster>();
+	
+	@Override
+	public void init() {
+		TextRPG.currnetStage = "BATTLE";
+	}
+	
+	private void setMonster() {
+		int playerLevel = Guild.getGuildPlayerByIndex(0).getLevel();
+		for(int i=0; i<Guild.getPartySize(); i++) {
+			monsterList.add(MonsterManager.makeMonsters(playerLevel));
+		}
+	}
+	
+	private void printBattleMain() {
+		String textTitle1 = String.format("====== Player ======\n");
+		IOManager.append(textTitle1);
+		for (int i = 0; i < Guild.getPartySize(); i++) {
+			IOManager.append(Guild.getGuildPartyByIndex(i).toString()+"\n");
+		}
+		String textTitle2 = String.format("====== Monster ======\n");
+		IOManager.append(textTitle2);
+		for (int i = 0; i < monsterList.size(); i++) {
+			IOManager.append(monsterList.get(i).toString()+"\n");
+		}
+	}
+	
+	@Override
+	public void start() {
+		setMonster();
+		printBattleMain();
+		try {
+			String input = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
