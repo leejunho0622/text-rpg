58548ccbbfb5b48b1fe22dc816599e3b69f076ed
leejunho0622 <lee49409780@gmail.com>
2024-11-18 17:49:10 +0900

    Move setUser() in LobbyStage class to TitleStage class

--- src/stages/LobbyStage.java
+++ src/stages/LobbyStage.java
@@ -1,42 +1,16 @@
 package stages;
 
-import java.io.IOException;
-
 import interfaces.IOManager;
 import textrpg.TextRPG;
-import units.Player;
 
 public class LobbyStage extends Stage{
 	@Override
 	public void init() {
 		TextRPG.currnetStage = "LOBBY";
 	}
-
-	public Player setUser() {
-		try {
-			String textTitle = String.format("====== TEXT RPG ======\n======= 직업 선택 =======\n[전사]\t[궁수]\t[마법사]\n");
-			IOManager.append(textTitle);
-			
-			String input = reader.readLine();
-			
-			if(input.equals("전사")) {
-				return new Player("플레이어", "전사", 1300, 100, 100, 100, true);
-			}
-			else if(input.equals("궁수")) {
-				return new Player("플레이어", "궁수", 1150, 250, 130, 70, true);
-			}
-			else if(input.equals("마법사")) {
-				return new Player("플레이어", "마법사", 1000, 400, 170, 30, true);
-			}
-			
-		} catch (IOException e) {}
-		return setUser();
-	}
 	
 	@Override
 	public void start() {
-		Guild.addGuildPlayer(setUser());
-		Guild.updateParty();
 		while(true) {
 			try {
 				String textTitle = String.format("====== TEXT RPG ======\n[전투]\t[정보]\t[상점]\n[길드]\n");
--- src/stages/TitleStage.java
+++ src/stages/TitleStage.java
@@ -1,14 +1,38 @@
 package stages;
 
+import java.io.IOException;
+
 import interfaces.IOManager;
 import textrpg.TextRPG;
+import units.Player;
 
 public class TitleStage extends Stage{
 	@Override
 	public void init() {
 		TextRPG.currnetStage = "TITLE";
 	}
-
+	
+	public Player setUser() {
+		try {
+			String textTitle = String.format("====== TEXT RPG ======\n======= 직업 선택 =======\n[전사]\t[궁수]\t[마법사]\n");
+			IOManager.append(textTitle);
+			
+			String input = reader.readLine();
+			
+			if(input.equals("전사")) {
+				return new Player("플레이어", "전사", 1300, 100, 100, 100, true);
+			}
+			else if(input.equals("궁수")) {
+				return new Player("플레이어", "궁수", 1150, 250, 130, 70, true);
+			}
+			else if(input.equals("마법사")) {
+				return new Player("플레이어", "마법사", 1000, 400, 170, 30, true);
+			}
+			
+		} catch (IOException e) {}
+		return setUser();
+	}
+	
 	@Override
 	public void start() {
 		while (true) {
@@ -28,5 +52,7 @@ public class TitleStage extends Stage{
 			
 			}catch (Exception e) {}
 		}
+		Guild.addGuildPlayer(setUser());
+		Guild.updateParty();
 	}
 }
