b8545fb430beb4d70a1991103bacee3a865804d4
leejunho0622 <lee49409780@gmail.com>
2024-11-18 14:28:03 +0900

    Add setUser()

--- src/stages/LobbyStage.java
+++ src/stages/LobbyStage.java
@@ -1,7 +1,10 @@
 package stages;
 
+import java.io.IOException;
+
 import interfaces.IOManager;
 import textrpg.TextRPG;
+import units.Player;
 
 public class LobbyStage extends Stage{
 	@Override
@@ -9,11 +12,33 @@ public class LobbyStage extends Stage{
 		TextRPG.currnetStage = "LOBBY";
 	}
 
+	public Player setUser() {
+		try {
+			String textTitle = String.format("====== TEXT RPG ======\n====== 직업 선택 ======\n[전사]\t[궁수]\t[마법사]\n");
+			IOManager.append(textTitle);
+			
+			String input = reader.readLine();
+			
+			if(input.equals("전사")) {
+				return new Player("플레이어", "전사", 1300, 100, 100, 100);
+			}
+			else if(input.equals("궁수")) {
+				return new Player("플레이어", "궁수", 1150, 250, 130, 70);
+			}
+			else if(input.equals("마법사")) {
+				return new Player("플레이어", "마법사", 1000, 400, 170, 30);
+			}
+			
+		} catch (IOException e) {}
+		return setUser();
+	}
+	
 	@Override
 	public void start() {
+		Guild.guildList.add(setUser());
 		while(true) {
 			try {
-				String textTitle = String.format("====== TEXT RPG ======\n[전투]\t[정보]\t[상점]\n");
+				String textTitle = String.format("====== TEXT RPG ======\n[전투]\t[정보]\t[상점]\n[길드]\n");
 				IOManager.append(textTitle);
 				
 				String input = reader.readLine();
