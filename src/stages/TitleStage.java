890e364b1cfb578539fa6a760c9e2be508286372
leejunho0622 <lee49409780@gmail.com>
2024-11-18 12:19:07 +0900

    Modify print Message in start()

--- src/stages/TitleStage.java
+++ src/stages/TitleStage.java
@@ -13,7 +13,7 @@ public class TitleStage extends Stage{
 	public void start() {
 		while (true) {
 			try {
-			String textTitle = String.format("=== TEXT RPG ===\n[1. 시작]\t[2. 종료]\n");
+			String textTitle = String.format("=== TEXT RPG ===\n[시작]\t[종료]\n");
 			IOManager.append(textTitle);
 			
 			String input = reader.readLine();
