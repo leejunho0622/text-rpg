6f22fcc1c6e09fb7d96b89360ff267244d9995d7
leejunho0622 <lee49409780@gmail.com>
2024-11-18 12:35:03 +0900

    Overrride init(), start() in LobbyStage class

--- src/stages/LobbyStage.java
+++ src/stages/LobbyStage.java
@@ -1,5 +1,37 @@
 package stages;
 
-public class LobbyStage extends Stage{
+import interfaces.IOManager;
+import textrpg.TextRPG;
 
+public class LobbyStage extends Stage{
+	@Override
+	public void init() {
+		TextRPG.currnetStage = "LOBBY";
+	}
+
+	@Override
+	public void start() {
+		while(true) {
+			try {
+				String textTitle = String.format("====== TEXT RPG ======\n[전투]\t[정보]\t[상점]\n");
+				IOManager.append(textTitle);
+				
+				String input = reader.readLine();
+				
+				if (input.equals("전투")) {
+					TextRPG.currnetStage = "BATTLE";
+					break;
+				}else if(input.equals("정보")) {
+					TextRPG.currnetStage = "ANY";
+					AnyStage.type = "INFO";
+					break;
+				}else if(input.equals("상점")) {
+					TextRPG.currnetStage = "ANY";
+					AnyStage.type = "SHOP";
+					break;
+				}
+				
+			}catch (Exception e) {}
+		}
+	}
 }
