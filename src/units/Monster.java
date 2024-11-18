43b60a9af47fcb9e9aaf39506e3b8fb7dccc292e
leejunho0622 <lee49409780@gmail.com>
2024-11-18 17:30:20 +0900

    Add attack()

--- src/units/Monster.java
+++ src/units/Monster.java
@@ -1,5 +1,7 @@
 package units;
 
+import interfaces.IOManager;
+
 public class Monster extends Unit{
 	public Monster(String name, int level, int hp, int damage, int def) {
 		super(name, level, hp, damage, def);
@@ -8,5 +10,14 @@ public class Monster extends Unit{
 	@Override
 	public String toString() {
 		return String.format("[%s][Lv. %d]\n[HP:%d/%d][%s:%d][%s:%d]", getName(), getLevel(), getHp(), getMaxHp(), "⚔️", getDamage(), "🛡️", getDef());
+	}
+	
+	public void attack(Player target) {
+		int att = getDamage() - target.getDef();
+		String textTitle = String.format("%s에게 %d의 데미지!\n", target.getName(), att);
+		IOManager.append(textTitle);
+		target.decreaseHp(att);
+		if(target.getHp() <= 0)
+			target.setHp(0);
 	}
 }
--- src/units/Player.java
+++ src/units/Player.java
@@ -1,5 +1,7 @@
 package units;
 
+import interfaces.IOManager;
+
 public class Player extends Unit{
 	public Player(String name, String job, int hp, int mp, int damage, int def, boolean party) {
 		super(name, job, hp, mp, damage, def, party);
@@ -7,6 +9,15 @@ public class Player extends Unit{
 	
 	@Override
 	public String toString() {
-		return String.format("[%s][%s][Lv. %d]\n[%d/%d][%d/%d][%s:%d][%s:%d]", getName(), getJob(), getLevel(), getHp(), getMaxHp(), getMp(), getMaxMp(), "⚔️", getDamage(), "🛡️", getDef());
+		return String.format("[%s][%s][Lv. %d]\n[HP:%d/%d][MP:%d/%d][%s:%d][%s:%d]", getName(), getJob(), getLevel(), getHp(), getMaxHp(), getMp(), getMaxMp(), "⚔️", getDamage(), "🛡️", getDef());
+	}
+	
+	public void attack(Monster target) {
+		int att = getDamage() - target.getDef();
+		String textTitle = String.format("%s에게 %d의 데미지!\n", target.getName(), att);
+		IOManager.append(textTitle);
+		target.decreaseHp(att);
+		if(target.getHp() <= 0)
+			target.setHp(0);
 	}
 }
