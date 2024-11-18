9f92390290f9966781d36a1be63b8afaf4f525ba
leejunho0622 <lee49409780@gmail.com>
2024-11-18 14:51:50 +0900

    Add stage class and Add class about Unit

---
+++ src/stages/AnyStage.java
@@ -0,0 +1,19 @@
+package stages;
+
+import textrpg.TextRPG;
+
+public class AnyStage extends Stage{
+	public static String type;
+	
+	@Override
+	public void init() {
+		TextRPG.currnetStage = "ANY";
+	}
+
+	@Override
+	public void start() {
+		if(type.equals("INFO")) {}
+		else if(type.equals("SHOP")) {}
+		else if(type.equals("GUILD")) {}
+	}
+}
---
+++ src/units/Slime.java
@@ -0,0 +1,7 @@
+package units;
+
+public class Slime extends Unit{
+	public Slime(String name, int level, int hp, int damage, int def) {
+		super(name, level, hp, damage, def);
+	}
+}
