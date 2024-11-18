84d4090ebb7c6feecf6549ec2c2d4c740bae0bd0
leejunho0622 <lee49409780@gmail.com>
2024-11-18 12:09:19 +0900

    Override init(), start()

--- src/stages/TitleStage.java
+++ src/stages/TitleStage.java
@@ -1,5 +1,32 @@
 package stages;
 
-public class TitleStage extends Stage{
+import interfaces.IOManager;
+import textrpg.TextRPG;
 
+public class TitleStage extends Stage{
+	@Override
+	public void init() {
+		TextRPG.currnetStage = "TITLE";
+	}
+
+	@Override
+	public void start() {
+		while (true) {
+			try {
+			String textTitle = String.format("=== TEXT RPG ===\n[1. 시작]\t[2. 종료]\n");
+			IOManager.append(textTitle);
+			
+			String input = reader.readLine();
+			
+			if (input.equals("시작")) {
+				TextRPG.currnetStage = "LOBBY";
+				break;
+			}else if(input.equals("종료")) {
+				TextRPG.playGame = false;
+				break;
+			}
+			
+			}catch (Exception e) {}
+		}
+	}
 }
