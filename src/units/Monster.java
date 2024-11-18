c171f661e6720b489bb413aecf5afdde5e5606e7
leejunho0622 <lee49409780@gmail.com>
2024-11-18 13:47:18 +0900

    Add contructors Player and Monster

---
+++ src/units/Monster.java
@@ -0,0 +1,7 @@
+package units;
+
+public class Monster extends Unit{
+	public Monster(String name, String job, int level, int hp, int damage, int def) {
+		super(name, job, level, hp, damage, def);
+	}
+}
---
+++ src/units/Player.java
@@ -0,0 +1,7 @@
+package units;
+
+public class Player extends Unit{
+	public Player(String name, String job, int level, int hp, int mp, int damage, int def, int exp) {
+		super(name, job, level, hp, mp, damage, def, exp);
+	}
+}
