d8532ddd4822426cde6918652132d19d6ee953eb
leejunho0622 <lee49409780@gmail.com>
2024-11-18 16:51:17 +0900

    Modify extends class

--- src/units/Slime.java
+++ src/units/Slime.java
@@ -1,6 +1,6 @@
 package units;
 
-public class Slime extends Unit{
+public class Slime extends Monster{
 	public Slime(String name, int level, int hp, int damage, int def) {
 		super(name, level, hp, damage, def);
 	}
