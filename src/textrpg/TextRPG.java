5f15bca76a56e51bd970a9bce0330c019a3a7525
leejunho0622 <lee49409780@gmail.com>
2024-11-18 11:21:27 +0900

    Add singleton pattern

--- src/textrpg/TextRPG.java
+++ src/textrpg/TextRPG.java
@@ -1,5 +1,13 @@
 package textrpg;
 
 public class TextRPG {
-
+	private TextRPG() {}
+	private static TextRPG instance = new TextRPG();
+	public static TextRPG getInstacne() {
+		return instance;
+	}
+	
+	public void run() {
+		
+	}
 }
