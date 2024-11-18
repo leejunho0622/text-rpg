79842af132022266a43fa57f8c84edfe97fe935e
leejunho0622 <lee49409780@gmail.com>
2024-11-18 16:52:42 +0900

    Overrride toString()

--- src/units/Monster.java
+++ src/units/Monster.java
@@ -4,4 +4,9 @@ public class Monster extends Unit{
 	public Monster(String name, int level, int hp, int damage, int def) {
 		super(name, level, hp, damage, def);
 	}
+	
+	@Override
+	public String toString() {
+		return String.format("[%s][Lv. %d]\n[HP:%d/%d][%s:%d][%s:%d]", getName(), getLevel(), getHp(), getMaxHp(), "⚔️", getDamage(), "🛡️", getDef());
+	}
 }
--- src/units/Player.java
+++ src/units/Player.java
@@ -4,4 +4,9 @@ public class Player extends Unit{
 	public Player(String name, String job, int hp, int mp, int damage, int def, boolean party) {
 		super(name, job, hp, mp, damage, def, party);
 	}
+	
+	@Override
+	public String toString() {
+		return String.format("[%s][%s][Lv. %d]\n[%d/%d][%d/%d][%s:%d][%s:%d]", getName(), getJob(), getLevel(), getHp(), getMaxHp(), getMp(), getMaxMp(), "⚔️", getDamage(), "🛡️", getDef());
+	}
 }
