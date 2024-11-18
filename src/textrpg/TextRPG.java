0c37a0672a56dc6d871556b2bf19693cd1972707
leejunho0622 <lee49409780@gmail.com>
2024-11-18 11:46:42 +0900

    Add init(), changeStage()

--- src/textrpg/TextRPG.java
+++ src/textrpg/TextRPG.java
@@ -1,13 +1,42 @@
 package textrpg;
 
-public class TextRPG {
+import java.util.HashMap;
+
+import interfaces.IOManager;
+import stages.Stage;
+import stages.TitleStage;
+import stages.LobbyStage;
+import stages.BattleStage;
+
+public class TextRPG implements IOManager{
 	private TextRPG() {}
 	private static TextRPG instance = new TextRPG();
 	public static TextRPG getInstacne() {
 		return instance;
 	}
 	
+	private HashMap<String, Stage> stageList = new HashMap<String, Stage>();
+	
+	public static String currnetStage = "";
+	public static String beforeStage = "";
+	
+	private void init() {
+		stageList.put("TITLE", new TitleStage());
+		stageList.put("LOBBY", new LobbyStage());
+		stageList.put("BATTLE", new BattleStage());
+		currnetStage = "TITLE";
+	}
+	
+	private void changeStage() {
+		String stageInfo = String.format("[ %s → %s ]\n", beforeStage, currnetStage);
+		IOManager.append(stageInfo);
+		Stage stage = stageList.get(currnetStage);
+		currnetStage = beforeStage;
+		stage.start();
+	}
+	
 	public void run() {
-		
+		init();
+		changeStage();
 	}
 }
