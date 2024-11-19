594e86af38d1a614d440b0dc72fbc0dabb814657
leejunho0622 <lee49409780@gmail.com>
2024-11-19 12:20:51 +0900

    Refactor : code refactoring

--- src/stages/LobbyStage.java
+++ src/stages/LobbyStage.java
@@ -3,39 +3,40 @@ package stages;
 import interfaces.IOManager;
 import textrpg.TextRPG;
 
-public class LobbyStage extends Stage{
+public class LobbyStage extends Stage {
 	@Override
 	public void init() {
 		TextRPG.currnetStage = "LOBBY";
 	}
-	
+
 	@Override
 	public void start() {
-		while(true) {
+		while (true) {
 			try {
 				String textTitle = String.format("====== TEXT RPG ======\n[전투]\t[정보]\t[상점]\n[길드]\n");
 				IOManager.append(textTitle);
-				
+
 				String input = reader.readLine();
-				
+
 				if (input.equals("전투")) {
 					TextRPG.currnetStage = "BATTLE";
 					break;
-				}else if(input.equals("정보")) {
+				} else if (input.equals("정보")) {
 					TextRPG.currnetStage = "ANY";
 					AnyStage.type = "INFO";
 					break;
-				}else if(input.equals("상점")) {
+				} else if (input.equals("상점")) {
 					TextRPG.currnetStage = "ANY";
 					AnyStage.type = "SHOP";
 					break;
-				}else if(input.equals("길드")) {
+				} else if (input.equals("길드")) {
 					TextRPG.currnetStage = "ANY";
 					AnyStage.type = "GUILD";
 					break;
 				}
-				
-			}catch (Exception e) {}
+
+			} catch (Exception e) {
+			}
 		}
 	}
 }
--- src/stages/TitleStage.java
+++ src/stages/TitleStage.java
@@ -6,53 +6,53 @@ import interfaces.IOManager;
 import textrpg.TextRPG;
 import units.Player;
 
-public class TitleStage extends Stage{
+public class TitleStage extends Stage {
 	private Guild guild = Guild.getInstance();
-	
+
 	@Override
 	public void init() {
 		TextRPG.currnetStage = "TITLE";
 	}
-	
+
 	public Player setUser() {
 		try {
 			String textTitle = String.format("====== TEXT RPG ======\n======= 직업 선택 =======\n[전사]\t[궁수]\t[마법사]\n");
 			IOManager.append(textTitle);
-			
+
 			String input = reader.readLine();
-			
-			if(input.equals("전사")) {
+
+			if (input.equals("전사")) {
 				return new Player("플레이어", "전사", 1300, 100, 100, 100, 5, true);
-			}
-			else if(input.equals("궁수")) {
+			} else if (input.equals("궁수")) {
 				return new Player("플레이어", "궁수", 1150, 250, 130, 70, 20, true);
-			}
-			else if(input.equals("마법사")) {
+			} else if (input.equals("마법사")) {
 				return new Player("플레이어", "마법사", 1000, 400, 170, 30, 10, true);
 			}
-			
-		} catch (IOException e) {}
+
+		} catch (IOException e) {
+		}
 		return setUser();
 	}
-	
+
 	@Override
 	public void start() {
 		while (true) {
 			try {
-			String textTitle = String.format("=== TEXT RPG ===\n[시작]\t[종료]\n");
-			IOManager.append(textTitle);
-			
-			String input = reader.readLine();
-			
-			if (input.equals("시작")) {
-				TextRPG.currnetStage = "LOBBY";
-				break;
-			}else if(input.equals("종료")) {
-				TextRPG.playGame = false;
-				break;
+				String textTitle = String.format("=== TEXT RPG ===\n[시작]\t[종료]\n");
+				IOManager.append(textTitle);
+
+				String input = reader.readLine();
+
+				if (input.equals("시작")) {
+					TextRPG.currnetStage = "LOBBY";
+					break;
+				} else if (input.equals("종료")) {
+					TextRPG.playGame = false;
+					break;
+				}
+
+			} catch (Exception e) {
 			}
-			
-			}catch (Exception e) {}
 		}
 		guild.addGuildPlayer(setUser());
 		guild.updateParty();
